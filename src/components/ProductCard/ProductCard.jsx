793916a038281dae9acf6188df32e5799cf8af93
Melya <melenko55@gmail.com>
2023-02-05 12:46:35 +0200

    Created Fridge Page

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,20 +1,17 @@
 import vegetablesIcon from "../../assets/vegetablesIcon.png";
 import "./ProductCard.css";
 
-const ProductCard = () => {
+const ProductCard = ({ product }) => {
+ 
   return (
     <div className="product-card">
       <div className="product-img-wrapper">
-        <img
-          className="product-img"
-          src="https://images.unsplash.com/photo-1546094096-0df4bcaaa337?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80"
-          alt=""
-        />
+        <img className="product-img" src={product.img} alt="" />
         <img className="category-icon" src={vegetablesIcon} />
       </div>
 
       <div className="product-info">
-        <h3 className="product-name">Tomatoes</h3>
+        <h3 className="product-name">{product.name}</h3>
         <h5 className="product-quantity">200 g</h5>
       </div>
     </div>
--- src/pages/Fridge/Fridge.jsx
+++ src/pages/Fridge/Fridge.jsx
@@ -1,34 +1,50 @@
-// Page with products that user has
 import ProductCard from "../../components/ProductCard/ProductCard";
-// It must contain the list of products and the quantity
-// Possibility to add a product
-// Possibility to delete a product
-// Possibility to edit a product
-// Possibility to view products by categories
-import "./Fridge.css"
+import "./Fridge.css";
+import { useEffect, useState } from "react";
 
 const Fridge = () => {
+  let filteredProducts = [];
+  const [products, setProducts] = useState([]);
+  const [category, setCategory] = useState("All");
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const response = await fetch("http://localhost:8080/products");
+      const data = await response.json();
+      setProducts(data);
+    };
+    getProducts();
+  }, []);
+
+  if (category !== "All") {
+    filteredProducts = products.filter(
+      (product) => product.category === category
+    );
+  } else {
+    filteredProducts = products;
+  }
+
   return (
     <div className="fridge">
       <h1 className="fridge-title">Fridge</h1>
-      <select name="category" id="category">
-        <option value="all">All</option>
-        <option value="meat">Meat</option>
-        <option value="vegetables">Vegetables</option>
-        <option value="fruits">Fruits</option>
-        <option value="dairy">Dairy</option>
+      <select
+        name="category"
+        id="category"
+        onChange={(e) => setCategory(e.target.value)}
+        value={category}
+      >
+        <option value="All">All</option>
+        <option value="Meat">Meat</option>
+        <option value="Vegetables">Vegetables</option>
+        <option value="Fruits">Fruits</option>
+        <option value="Dairy">Dairy</option>
       </select>
       <button className="add-btn">Add Product</button>
       <div className="products">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        {filteredProducts.length > 0 &&
+          filteredProducts.map((product) => {
+            return <ProductCard product={product} key={product.id} />;
+          })}
       </div>
     </div>
   );
